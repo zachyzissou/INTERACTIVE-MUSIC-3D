baf816697838afde0f834bfd3b34e5851b0e5f9e
zachyzissou <zachgonser@me.com>
2025-06-22 22:10:16 -0500

    style: revert assertPrimitives to main

--- src/lib/assertPrimitives.ts
+++ src/lib/assertPrimitives.ts
@@ -6,7 +6,9 @@ export function assertPrimitives(obj: any, path = 'root') {
         if (Array.isArray(val)) {
           val.forEach((v, i) => assertPrimitives(v, `${current}[${i}]`))
         } else if (Object.getPrototypeOf(val) !== Object.prototype) {
-          throw new Error(`\u26d4\ufe0f Non-plain value at ${current}: ${(val as any).constructor.name}`)
+          throw new Error(`\u26d4\ufe0f Non-plain value at ${current}: ${
+            (val as any).constructor.name
+          }`)
         } else {
           assertPrimitives(val, current)
         }
