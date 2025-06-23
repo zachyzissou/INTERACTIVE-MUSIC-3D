efbebf8d1a3a6f7b454e17e481af2dcb46b90c45
zachyzissou <zachgonser@me.com>
2025-06-22 20:22:49 -0500

    Fix assertPrimitives syntax

---
+++ src/lib/assertPrimitives.ts
@@ -0,0 +1,16 @@
+export function assertPrimitives(obj: any, path = 'root') {
+  if (obj && typeof obj === 'object') {
+    for (const [key, val] of Object.entries(obj)) {
+      const current = `${path}.${key}`
+      if (val && typeof val === 'object') {
+        if (Array.isArray(val)) {
+          val.forEach((v, i) => assertPrimitives(v, `${current}[${i}]`))
+        } else if (Object.getPrototypeOf(val) !== Object.prototype) {
+          throw new Error(`\u26d4\ufe0f Non-plain value at ${current}: ${(val as any).constructor.name}`)
+        } else {
+          assertPrimitives(val, current)
+        }
+      }
+    }
+  }
+}
